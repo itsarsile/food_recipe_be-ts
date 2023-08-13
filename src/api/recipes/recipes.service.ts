e8e0362dad402e19f10dafa4856972af392b12c3
Arsile <ajiprio@hotmail.com>
2023-08-13 19:09:20 +0700

    feat(Recipes API): :sparkles: Added CRUD for Recipes

--- src/api/recipes/recipes.service.ts
+++ src/api/recipes/recipes.service.ts
@@ -1,7 +1,24 @@
+import { eq } from "drizzle-orm";
 import { CreateRecipe } from "../../../types";
 import db from "../../db";
 import { recipe } from "../../db/schema";
 
 export const createRecipe = async (input: CreateRecipe) => {
   return await db.insert(recipe).values(input);
+};
+
+export const getAllRecipes = async () => {
+  return await db.select().from(recipe);
+};
+
+export const updateRecipe = async (input: Object, recipeId: number) => {
+  return await db.update(recipe).set(input).where(eq(recipe.id, recipeId));
+};
+
+export const getRecipeById = async (recipeId: number) => {
+  return await db.select().from(recipe).where(eq(recipe.id, recipeId));
+};
+
+export const deleteRecipeById = async (recipeId: number) => {
+  return await db.delete(recipe).where(eq(recipe.id, recipeId));
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
-import express, { Application } from 'express'
-import userRoutes from './api/users'
-import authRoutes from './api/auth'
-import passport from 'passport'
+import express, { Application } from "express";
+import userRoutes from "./api/users";
+import authRoutes from "./api/auth";
+import recipeRoutes from "./api/recipes";
 
-const app: Application = express()
+const app: Application = express();
 
-app.use('/api/users', userRoutes)
-app.use('/api/auth', authRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/recipes", recipeRoutes);
 
-export default app
+export default app;
